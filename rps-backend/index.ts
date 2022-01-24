8826de162ebc73b4b047686d763b216a5218b0be
Veikka <32365239+SirVeggie@users.noreply.github.com>
2022-01-24 17:51:52 +0200

    Update index.ts

--- rps-backend/index.ts
+++ rps-backend/index.ts
@@ -10,8 +10,7 @@ import path from 'path';
 const app = express();
 
 app.use(express.json());
-app.use(express.static(path.join(__dirname, 'build')));
-// app.use(express.static('build'));
+app.use(express.static('build'));
 app.use(cors());
 app.use(morgan('tiny'));
 
